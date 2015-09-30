eabe39a0b1737fb2f29a99df96cb828e890b4d22
Brian Whigham <github.com.brianwhigham.lib.canmailme@itmonger.com>
2015-09-29 22:47:24 -0400

    oops, the startup.sh is important

---
+++ startup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+test -z $FOLLOW_LOG_FILE && FOLLOW_LOG_FILE=/var/log/syslog
+test -z $UDP_PORT && UDP_PORT=514
+test -z $TCP_PORT && TCP_PORT=514
+test -z $LOG_GROUP_NAME && LOG_GROUP_NAME=awslogs
+test -z $LOG_STREAM_NAME && LOG_STREM_NAME=syslog
+test -z $AWS_REGION && AWS_REGION="us-east-1"
+test -z $BUFFER_DURATION && BUFFER_DURATION="5000"
+test -z $INITIAL_POSITION && INITIAL_POSITION="start_of_file"
+test -z $DATETIME_FORMAT && DATETIME_FORMAT="%Y-%m-%d %H:%M:%S"
+
+sed -i "s/#\$ModLoad imudp/\$ModLoad imudp/" /etc/rsyslog.conf && \
+sed -i "s/#\$UDPServerRun $UDP_PORT/\$UDPServerRun $UDP_PORT/" /etc/rsyslog.conf && \
+sed -i "s/#\$ModLoad imtcp/\$ModLoad imtcp/" /etc/rsyslog.conf && \
+sed -i "s/#\$InputTCPServerRun $TCP_PORT/\$InputTCPServerRun $TCP_PORT/" /etc/rsyslog.conf
+
+/etc/init.d/rsyslog restart
+
+cat - > /awslogs.conf <<EOF
+[general]
+state_file = /var/awslogs/state/agent-state
+
+[syslog]
+datetime_format = $DATETIME_FORMAT
+file = $FOLLOW_LOG_FILE
+buffer_duration = $BUFFER_DURATION
+log_stream_name = $LOG_STREAM_NAME
+initial_position = $INITIAL_POSITION
+log_group_name = $LOG_GROUP_NAME
+EOF
+
+python ./awslogs-agent-setup.py -n -r $AWS_REGION -c /awslogs.conf
+
+/usr/local/bin/supervisord
+
